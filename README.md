474aec35541298ecc3dd0a6c272c79e06dfb2568
Goldstack Template Build <public@pureleap.com>
2022-02-15 21:59:54 +0000

    Update boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# nextjs-bootstrap-boilerplate
-Boilerplate for a Next.js Project using Bootstrap Styling
+# Goldstack Project Root
+
+The root of the project contains configuration shared across modules as well as various utility scripts for applying operations for all modules.
+
+You can find the modules you have selected in the folder `packages/`.
+
+Find further information here:
+
+- [Project Configuration](https://docs.goldstack.party/docs/goldstack/configuration)
+- [Getting Started](https://docs.goldstack.party/docs/goldstack/getting-started)
+- [Security Hardening](https://docs.goldstack.party/docs/goldstack/security-hardening)
---
+++ packages/app-nextjs-bootstrap-1/next.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const getLocalPackages = require('./scripts/getLocalPackages');
+
+const localPackages = getLocalPackages.getLocalPackages();
+const withTM = require('next-transpile-modules')(localPackages);
+
+const nextConfig = {
+  webpack: (config, options) => {
+    return config;
+  },
+  eslint: {
+    // ESLint managed on the workspace level
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true,
+  },
+};
+
+const config = withPlugins(
+  [
+    [withTM()],
+    [
+      optimizedImages,
+      {
+        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig
+);
+
+module.exports = config;
