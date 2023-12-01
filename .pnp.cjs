6ebe1de3aa5bd9a1ffdc12e4afdb6e3a73f94b53
Goldstack Project Builder <goldstack@pureleap.com>
2023-12-01 22:32:15 +0000

    Automated update of generated boilerplate by goldstack.party

--- .pnp.cjs
+++ .pnp.cjs
@@ -55,10 +55,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@adobe/css-tools", [\
-        ["npm:4.3.1", {\
-          "packageLocation": "./.yarn/cache/@adobe-css-tools-npm-4.3.1-8afe87e987-ad43456379.zip/node_modules/@adobe/css-tools/",\
+        ["npm:4.3.2", {\
+          "packageLocation": "./.yarn/cache/@adobe-css-tools-npm-4.3.2-779f6c743d-9667d61d55.zip/node_modules/@adobe/css-tools/",\
           "packageDependencies": [\
-            ["@adobe/css-tools", "npm:4.3.1"]\
+            ["@adobe/css-tools", "npm:4.3.2"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -1593,7 +1593,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@goldstack/utils-cli", "npm:0.2.4"],\
             ["@goldstack/utils-docs", "npm:0.3.11"],\
             ["@goldstack/utils-sh", "npm:0.4.7"],\
-            ["@types/yargs", "npm:15.0.17"],\
+            ["@types/yargs", "npm:15.0.19"],\
             ["source-map-support", "npm:0.5.21"],\
             ["yargs", "npm:15.4.1"]\
           ],\
@@ -2998,7 +2998,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@testing-library-jest-dom-npm-5.16.5-b6d1f4e02f-94911f901a.zip/node_modules/@testing-library/jest-dom/",\
           "packageDependencies": [\
             ["@testing-library/jest-dom", "npm:5.16.5"],\
-            ["@adobe/css-tools", "npm:4.3.1"],\
+            ["@adobe/css-tools", "npm:4.3.2"],\
             ["@babel/runtime", "npm:7.11.2"],\
             ["@types/testing-library__jest-dom", "npm:5.9.2"],\
             ["aria-query", "npm:5.0.0"],\
@@ -3496,10 +3496,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@types/yargs", [\
-        ["npm:15.0.17", {\
-          "packageLocation": "./.yarn/cache/@types-yargs-npm-15.0.17-c303262f60-b306af8c32.zip/node_modules/@types/yargs/",\
+        ["npm:15.0.19", {\
+          "packageLocation": "./.yarn/cache/@types-yargs-npm-15.0.19-6387136847-6a509db363.zip/node_modules/@types/yargs/",\
           "packageDependencies": [\
-            ["@types/yargs", "npm:15.0.17"],\
+            ["@types/yargs", "npm:15.0.19"],\
             ["@types/yargs-parser", "npm:15.0.0"]\
           ],\
           "linkType": "HARD"\
@@ -4108,7 +4108,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["archiver", "npm:5.3.2"],\
             ["archiver-utils", "npm:2.1.0"],\
-            ["async", "npm:3.2.4"],\
+            ["async", "npm:3.2.5"],\
             ["buffer-crc32", "npm:0.2.13"],\
             ["readable-stream", "npm:3.6.0"],\
             ["readdir-glob", "npm:1.1.3"],\
@@ -4257,10 +4257,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "HARD"\
         }],\
-        ["npm:3.2.4", {\
-          "packageLocation": "./.yarn/cache/async-npm-3.2.4-aba13508f9-43d07459a4.zip/node_modules/async/",\
+        ["npm:3.2.5", {\
+          "packageLocation": "./.yarn/cache/async-npm-3.2.5-f5dbdabdfc-5ec77f1312.zip/node_modules/async/",\
           "packageDependencies": [\
-            ["async", "npm:3.2.4"]\
+            ["async", "npm:3.2.5"]\
           ],\
           "linkType": "HARD"\
         }]\
