59d7eb195eb4589452f51b158df3f8a8850445d3
Goldstack Project Builder <1448524+mxro@users.noreply.github.com>
2026-04-09 03:43:24 +0000

    Automated update of generated boilerplate by goldstack.party

--- .pnp.cjs
+++ .pnp.cjs
@@ -2656,13 +2656,13 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@goldstack/template-nextjs", [\
-      ["npm:0.5.105", {\
-        "packageLocation": "./.yarn/cache/@goldstack-template-nextjs-npm-0.5.105-3ab629de30-d6ebd7c1fc.zip/node_modules/@goldstack/template-nextjs/",\
+      ["npm:0.5.106", {\
+        "packageLocation": "./.yarn/cache/@goldstack-template-nextjs-npm-0.5.106-f07c8ae061-e97d86adfd.zip/node_modules/@goldstack/template-nextjs/",\
         "packageDependencies": [\
           ["@goldstack/infra", "npm:0.4.41"],\
           ["@goldstack/infra-aws", "npm:0.4.65"],\
-          ["@goldstack/template-nextjs", "npm:0.5.105"],\
-          ["@goldstack/template-static-website-aws", "npm:0.5.92"],\
+          ["@goldstack/template-nextjs", "npm:0.5.106"],\
+          ["@goldstack/template-static-website-aws", "npm:0.5.93"],\
           ["@goldstack/utils-aws-cli", "npm:0.4.66"],\
           ["@goldstack/utils-aws-lambda", "npm:0.3.78"],\
           ["@goldstack/utils-cli", "npm:0.3.32"],\
@@ -2682,12 +2682,13 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@goldstack/template-static-website-aws", [\
-      ["npm:0.5.92", {\
-        "packageLocation": "./.yarn/cache/@goldstack-template-static-website-aws-npm-0.5.92-0d9bb37727-8734ee2251.zip/node_modules/@goldstack/template-static-website-aws/",\
+      ["npm:0.5.93", {\
+        "packageLocation": "./.yarn/cache/@goldstack-template-static-website-aws-npm-0.5.93-75e58d17ed-6f9190f300.zip/node_modules/@goldstack/template-static-website-aws/",\
         "packageDependencies": [\
           ["@goldstack/infra", "npm:0.4.41"],\
           ["@goldstack/infra-aws", "npm:0.4.65"],\
-          ["@goldstack/template-static-website-aws", "npm:0.5.92"],\
+          ["@goldstack/template-static-website-aws", "npm:0.5.93"],\
+          ["@goldstack/utils-aws-cli", "npm:0.4.66"],\
           ["@goldstack/utils-cli", "npm:0.3.32"],\
           ["@goldstack/utils-config", "npm:0.4.41"],\
           ["@goldstack/utils-log", "npm:0.3.34"],\
@@ -5847,7 +5848,7 @@ const RAW_RUNTIME_STATE =
       ["workspace:packages/app-nextjs-bootstrap-1", {\
         "packageLocation": "./packages/app-nextjs-bootstrap-1/",\
         "packageDependencies": [\
-          ["@goldstack/template-nextjs", "npm:0.5.105"],\
+          ["@goldstack/template-nextjs", "npm:0.5.106"],\
           ["@goldstack/utils-sh", "npm:0.5.39"],\
           ["@swc/core", "virtual:16ea8aa1a6e3d7e3caad7a728129134ccf425225f70e02114f24bbb7b9874cedac04c00dafd9c218b946bfbd84b030abb0b024f90a0bd869ff619a92b51d4834#npm:1.15.8"],\
           ["@swc/jest", "virtual:16ea8aa1a6e3d7e3caad7a728129134ccf425225f70e02114f24bbb7b9874cedac04c00dafd9c218b946bfbd84b030abb0b024f90a0bd869ff619a92b51d4834#npm:0.2.39"],\
